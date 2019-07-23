dee747a3d46acae3717e6a8cf63a70d31effdc3a
Andrey <streetd1rt@yandex.ru>
2019-07-23 17:24:15 +0300

    upd

--- src/components/layouts/main-menu.js
+++ src/components/layouts/main-menu.js
@@ -4,7 +4,11 @@ class MainMenu extends React.Component {
   render() {
     return (
       <div>
-        
+          <div className="main-menu">
+            <a href="#" className="main-menu__logo">
+              <img src="" alt=""/>
+            </a>
+          </div>
       </div>
     )
   }
