7cc2787fe0818c70ec96e21abacf3b70b9f189f2
Andrey <streetd1rt@yandex.ru>
2019-07-25 17:12:39 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import './components/blocks/common/index.sass'
+import './components/common/index.sass'
+import './components/common/variables.sass'
+import './assets/fonts/index.sass'
 import MainMenu from './components/blocks/main-menu/index'
 import Chat from './components/layouts/chat/index'
 
--- src/components/layouts/chat/dialogs-list/index.js
+++ src/components/layouts/chat/dialogs-list/index.js
@@ -9,13 +9,13 @@ class Dialogs extends React.Component {
     return (
       <div className="dialogs-list">
         <div className="dialogs-list__head">
-          <div class="dialogs-list__title-wrap">
-            <div class="dialogs-list__title">Сообщения</div>
-            <div class="dialogs-list__count"><span>1</span></div>
+          <div className="dialogs-list__title-wrap">
+            <div className="dialogs-list__title">Сообщения</div>
+            <div className="dialogs-list__count"><span>1</span></div>
           </div>       
           <div className="dialogs-list__new-chat">+ Новый чат</div>
         </div>
-        <div class="dialogs-list__search">
+        <div className="dialogs-list__search">
           <div className="input-box">
             <input type="text" className="dialogs-list__input input-main" placeholder="Поиск диалогов..."/> 
           </div>
@@ -36,8 +36,8 @@ class Dialogs extends React.Component {
             <div className="dialogs-message__qute">
               <div className="dialogs-message__qute-text">Qute.agency</div>
               <div className="dialogs-message__qute-images">
-                <img src={avatar} class="dialogs-message__qute-image" alt="avatar"/>
-                <img src={avatar2} class="dialogs-message__qute-image" alt="avatar"/>
+                <img src={avatar} className="dialogs-message__qute-image" alt="avatar"/>
+                <img src={avatar2} className="dialogs-message__qute-image" alt="avatar"/>
               </div>
             </div>
           </div>
@@ -53,7 +53,7 @@ class Dialogs extends React.Component {
               </div>
             </div>
           </div>
-          <div className="dialogs-message__item dialogs-message__item--active">
+          <div className="dialogs-message__item">
             <div className="dialogs-message__inner">
               <div className="dialogs-message__avatar">
                 <img src={avatar} alt="avatar"/>
@@ -68,9 +68,9 @@ class Dialogs extends React.Component {
             <div className="dialogs-message__qute">
               <div className="dialogs-message__qute-text">Qute.agency</div>
               <div className="dialogs-message__qute-images">
-                <img src={avatar2} class="dialogs-message__qute-image" alt="avatar"/>
-                <img src={avatar3} class="dialogs-message__qute-image" alt="avatar"/>
-                <img src={avatar} class="dialogs-message__qute-image" alt="avatar"/>
+                <img src={avatar2} className="dialogs-message__qute-image" alt="avatar"/>
+                <img src={avatar3} className="dialogs-message__qute-image" alt="avatar"/>
+                <img src={avatar} className="dialogs-message__qute-image" alt="avatar"/>
               </div>
             </div>
           </div>
