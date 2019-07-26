720078dbd45cfd1bbaee1dd2d81096bf9b6a89e3
Andrey <streetd1rt@yandex.ru>
2019-07-26 15:21:40 +0300

    upd

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './common/index.sass'
+import './common/variables.sass'
+import '../assets/fonts/index.sass'
+// import MainMenu from './blocks/main-menu/index'
+// import Chat from './layouts/chat/index'
+
+class App extends React.Component {
+  render () {
+    return (
+     <div>
+       <div className="content-wrapper">
+        {/* <MainMenu />
+        <Chat /> */}
+       </div>
+     </div>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 serviceWorker.unregister();
