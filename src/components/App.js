926975ecafe030298920032dd1f097207733495d
Fryder <streetd1rt@yandex.ru>
2019-07-31 00:11:52 +0300

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './common/index.sass'
 import './common/variables.sass'
 import '../assets/fonts/index.sass'
-// import MainMenu from './blocks/main-menu/index'
-// import Chat from './layouts/chat/index'
+import MainMenu from './blocks/main-menu/index'
+import Chat from './layouts/chat/index'
 
 class App extends React.Component {
   render () {
     return (
      <div>
        <div className="content-wrapper">
-        {/* <MainMenu />
-        <Chat /> */}
+        <MainMenu />
+        <Chat />
        </div>
      </div>
     )
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './index.sass';
 
 class Login extends React.Component {
   render () {
     return (
-      <div className="div">
-          Login
-      </div>
+      <>
+        <div className="main">
+          <div className="main__wrapper">
+            <form className="form">
+              <h2 className="form__title">Авторизация</h2>
+              <input className="form__input" type="text" name="login" placeholder="Логин" required/>
+              <input className="form__input" type="password" name="password" placeholder="Пароль" required/>
+              <button className="form__button">Войти</button>
+            </form>
+          </div>
+        </div>
+      </>
     )
   }
 }
